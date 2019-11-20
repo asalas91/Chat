a36b79c98bf8acbde9a8adb9cf287fafa9cf6f2e
Zolyder <vr.luis25@gmail.com>
2019-11-19 23:13:20 -0600

    PLA-ANG6 - Integración de imagenes para status de usuarios

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -8,9 +8,10 @@ export class UserService {
   friends: User[];
   constructor() {
     const users: User[] = [
-      {nick: 'Eduardo', subnick: 'Mi mensaje personal', age: 28, email: 'eduardo@platzi.com', friend: true, uid: 1},
-      {nick: 'Yuliana', subnick: 'Mi mensaje personal', age: 25, email: 'yuliana@platzi.com', friend: true, uid: 2},
-      {nick: 'Freddy', subnick: 'Mi mensaje personal', age: 28, email: 'freddy@platzi.com', friend: false, uid: 3}
+      {nick: 'Eduardo', subnick: 'Mi mensaje personal', age: 28, email: 'eduardo@platzi.com', friend: true, status: 'online', uid: 1},
+      {nick: 'Yuliana', subnick: 'Mi mensaje personal', age: 25, email: 'yuliana@platzi.com', friend: true, status: 'offline', uid: 2},
+      {nick: 'Freddy', subnick: 'Mi mensaje personal', age: 28, email: 'freddy@platzi.com', friend: false, status: 'busy', uid: 3},
+      {nick: 'Luis Carlos', subnick: 'Mi mensaje personal', age: 20, email: 'luis@platzi.com', friend: false, status: 'away', uid: 4}
     ];
     this.friends = users;
   }
