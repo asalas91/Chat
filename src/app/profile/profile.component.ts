e92b13651217b68671f0e2f6a2d87f3bc2492803
Luis Carlos Villa <vr.luis25@gmail.com>
2020-03-02 20:35:05 -0600

    PLA-ANG6 | Guardando las imagenes de perfil en nuestra base de datos de firebase

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,6 +3,7 @@ import { User } from 'firebase';
 import { AuthenticationService } from '../services/authentication.service';
 import { UserService } from '../services/user.service';
 import { ImageCroppedEvent } from 'ngx-image-cropper';
+import { AngularFireStorage } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-profile',
@@ -13,9 +14,11 @@ export class ProfileComponent implements OnInit {
   user: User;
   imageChangedEvent: any = '';
   croppedImage: any = '';
+  picture: any;
   constructor(
       private userService: UserService,
-      private authenticationService: AuthenticationService
+      private authenticationService: AuthenticationService,
+      private firebaseStorage: AngularFireStorage
     ) {
       this.authenticationService.getStatus().subscribe((status) => {
         this.userService.getUserById(status.uid).valueChanges().subscribe((data: User) => {
@@ -29,12 +32,33 @@ export class ProfileComponent implements OnInit {
       });
     }
 
+  ngOnInit() {
+  }
   saveSettings() {
-    this.userService.editUser(this.user).then(() => {
-      alert('Camgios guardados');
-    }).catch((error) => {
-      alert('Hubo un error');
-    });
+    if (this.croppedImage) {
+      const currentPictureId = Date.now();
+      const pictures = this.firebaseStorage.ref('pictures/' + currentPictureId + '.jpg').putString(this.croppedImage, 'data_url');
+      pictures.then((result) => {
+        this.picture = this.firebaseStorage.ref('pictures/' + currentPictureId + '.jpg').getDownloadURL();
+        this.picture.subscribe((p) => {
+          this.userService.setAvatar(p, this.user.uid).then(() => {
+            alert('Avatar subido correctamente');
+          }).catch((error) => {
+            alert('Hubo un error al tratar de subir la imagen');
+            console.log('Error :', error);
+          });
+        });
+      }).catch((error) => {
+        console.log(error);
+      });
+    } else {
+      this.userService.editUser(this.user).then(() => {
+        alert('Cambios guardados');
+      }).catch((error) => {
+        alert('Hubo un error');
+        console.log('Error :', error);
+      });
+    }
   }
 
   fileChangeEvent(event: any): void {
@@ -51,9 +75,6 @@ export class ProfileComponent implements OnInit {
   }
   loadImageFailed() {
       // show message
-  }
-
-  ngOnInit() {
   }
 
 }
